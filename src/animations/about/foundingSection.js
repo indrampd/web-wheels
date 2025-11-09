d540f748b4d5154b08eca5efbb9b573932792046
indrampd <indrampd21@gmail.com>
2025-11-09 08:31:47 +0700

    Refactor founding section animation to improve update handling and remove commented-out code

--- src/animations/about/foundingSection.js
+++ src/animations/about/foundingSection.js
@@ -11,6 +11,7 @@ export function foundingSectionAnimation() {
 	gsap.context((self) => {
 		let velocity = 0;
 		let direction = 1;
+		let isFirstUpdate = true;
 		const titles = self.selector(".founding_title");
 		const animateSkewX = gsap.quickTo(titles, "skewX", {
 			duration: 0.4,
@@ -30,13 +31,13 @@ export function foundingSectionAnimation() {
 				end: "bottom bottom",
 				scrub: true,
 				onUpdate: (self) => {
+					if (isFirstUpdate) {
+						isFirstUpdate = false;
+						return;
+					}
+
 					velocity = self.getVelocity() / 100;
 					direction = self.direction;
-
-					// titles.forEach((title) => {
-					// 	title.style.transformOrigin =
-					// 		direction === 1 ? "top center" : "bottom center";
-					// });
 
 					skewValue = gsap.utils.mapRange(
 						0,
@@ -57,31 +58,7 @@ export function foundingSectionAnimation() {
 				},
 			});
 
-			// lenis.on("scroll", (e) => {
-			// 	velocity = e.velocity;
-			// 	direction = e.direction;
-
-			// 	skewValue = gsap.utils.mapRange(
-			// 		0,
-			// 		20,
-			// 		0,
-			// 		6,
-			// 		Math.abs(velocity)
-			// 	);
-			// 	setSkewX(skewValue * direction);
-			// });
-
-			// lenis.on("scrollEnd", () => {
-			// 	gsap.to(gVisualWrap, {
-			// 		skewX: 0,
-			// 		ease: "power3.out",
-			// 		duration: 1,
-			// 	});
-			// });
-
 			return () => {
-				// lenis.off("scroll");
-				// lenis.off("scrollEnd");
 				gsap.killTweensOf(titles);
 				ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
 			};
